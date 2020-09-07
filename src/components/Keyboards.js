accbc494e104f19730c0146f40a120c7489a5442
Jeff Rock <SunShineOneX@gmail.com>
2020-09-06 21:25:22 -0400

    fixed games and keyboards large errors with spacing -- formatted file

--- src/components/Keyboards.js
+++ src/components/Keyboards.js
@@ -1,6 +1,4 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import "../styles/keyboards.css";
 
@@ -15,9 +13,7 @@ class Keyboards extends Component {
   async getKeyboardInfo() {
     try {
       const res = await axios.get("http://localhost:8080/keyboards");
-      this.setState({
-        info: res.data
-      }); // remove json if issue
+      this.setState({ info: res.data }); // remove json if issue
       console.log(this.state.info);
     } catch {
       console.log("error");
@@ -29,59 +25,31 @@ class Keyboards extends Component {
   }
 
   render() {
-    return ( <
-      div className = "container-fluid"
-      id = "card__grid" >
-
-      <
-      h1 > Mechanical Keyboards < /h1> <
-      div class = "row" > {
-        this.state.info.map((keyboard) => {
-          return ( <
-            div class = "card col-lg-3 col-md-4" >
-            <
-            img class = "card-img-top"
-            src = {
-              keyboard.url
-            }
-            onMouseOver = {
-              e => (e.currentTarget.src = `${keyboard.webUrl}`)
-            }
-            onMouseOut = {
-              e => (e.currentTarget.src = `${keyboard.url}`)
-            }
-            alt = "Card image cap" / >
-            <
-            div class = "card-body" >
-            <
-            h5 class = "card-title" > {
-              keyboard.model
-            } < /h5> <
-            h6 class = "card-subtitle" > {
-              keyboard.manufacturer
-            } < /h6> <
-            p class = "card-text" > {
-              keyboard.description
-            } < /p> <
-            p class = "card-text" > < b > {
-              keyboard.price
-            } < /b></p >
-            <
-            a href = "#"
-            class = "btn btn-primary" > Add to cart < /a> <
-            /div> <
-            /div>    
-          );
-        })
-      } <
-      /div>
-
-      <
-      /div>
-
-
-    )
-  }
-}
+    return (
+      <div className="container-fluid" id="card__grid">
+        
+        <h1>Mechanical Keyboards</h1>
+          <div class="row">
+          {this.state.info.map((keyboard) => {
+          return (
+            <div class="card col-lg-3 col-md-4">
+  <img class="card-img-top" src={keyboard.url}  onMouseOver={e => (e.currentTarget.src =`${keyboard.webUrl}`)} onMouseOut={e => (e.currentTarget.src =`${keyboard.url}`)}alt="Card image cap" />
+  <div class="card-body">
+    <h5 class="card-title">{keyboard.model}</h5>
+    <h6 class="card-subtitle">{keyboard.manufacturer}</h6>
+    <p class="card-text">{keyboard.description}</p>
+    <p class="card-text"><b>{keyboard.price}</b></p>
+    <a href="#" class="btn btn-primary">Add to cart</a>
+  </div>
+  </div>    
+        );
+        })}
+      </div>
+      
+      </div>
+    
+     
+    )}
+    }
 
 export default Keyboards;
