10aaa5b207a3307d18dc4f6cee52a6cae4924b07
Jeffrey Rockenbach <SunShineOneX@gmail.com>
2020-11-05 08:28:29 -0500

    quick little upd

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "../styles/home.css";
 
 function Home() {
+  // quick comment test
   alert("Please allow 15 seconds for my heroku deployed back end to wake up! Thank you.")
     return (
         <div className="container-fluid">
